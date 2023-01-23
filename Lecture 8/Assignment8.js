3df10af3ea2001c02b8b2fa616933f9c5a3e361a
Ankush Ladani <astarladani@gmail.com>
2023-01-23 09:20:15 +0530

    Assignment 8 Done

---
+++ Lecture 8/Assignment8.js
@@ -0,0 +1,43 @@
+// 1. How do you create Nested Routes react-router-dom cofiguration
+// By intoroducing a route inside children of children.
+
+// Ex. /login/market
+// Then we will first build /login path under home in children array and Outlet will contain all children routes.
+// Then we will introduce another children array in which we define path like market and we define outlet in the parent which is Login Component.
+
+// 2. What is the order of life cycle method calls in Class Based Components
+
+// This order is for only a single component and not nested components
+
+// First constructor will be created
+// Then render method will be called
+// Then componentDidMount method will be called
+// Then componentWillUpdate will be called
+// And finally componentWillUnmount will be called
+
+// 3. Why do we use componentDidMount?
+// The componentDidMount() method allows us to execute the React code when the component is already placed in the DOM (Document Object Model). This method is called during the Mounting phase of the React Life-cycle i.e after the component is rendered.
+
+// 4. Why do we use componentWillUnmount? Show with example
+// We have to use this method for performance optimization.
+// For Ex. We have created a Interval function which will fetch data on specific page after specific time. In this cases if we will not use this method then due to SPA architecture , it will be called again and again on other pages too and after some time it will heavily loaded and slow down our page.
+
+// In these type of cases we should clear that Interval in this method.
+
+// 5. (Research) Why do we use super(props) in constructor?
+// https://www.turing.com/kb/beginners-guide-to-super-and-super-props-in-react -> Link
+// Super() function is to call the constructor of the parent class. It is used when we need to access a few variables in the parent class.
+// super() is used to call the parent constructor, whereas, super(props) would pass the props to the parent constructor.
+// * React assigns props on the instance right after calling the component constructor.
+
+// 6. (Research) Why can't we have the callback function of useEffect async?
+
+// https://www.designcise.com/web/tutorial/why-cant-react-useeffect-callback-be-async
+
+// because:
+// 1. async functions implicitly return a promise, and;
+// 2. useEffect expects its callback to either return nothing or a clean-up function
+
+// Therefore, to use async/await inside the useEffect hook, you can do either of the following:
+// Create a Self-Invoking Anonymous Function;
+// Create a Nested Named Function.

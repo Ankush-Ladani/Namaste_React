626db59e1eac9997eb235b167e51eb2f834e92bf
Ankush Ladani <astarladani@gmail.com>
2023-01-29 16:00:51 +0530

    Assignment 10 Done

---
+++ Lecture 10/Assignment10.js
@@ -0,0 +1,17 @@
+// 1. Explore all the ways of writing css.
+
+// - normal style.css file and do styling using classes and id's.
+// - Inline styling
+// - using MaterialUI , baseUI , etc
+// - Tailwind CSS
+// - SASS or SCSS
+
+// 2. In tailwind.config.js, what does all the keys mean (content, theme, extend, plugins)?
+// So basically these all are for customization of our choices.
+// content -> It basically defines the path to which tailwind will work.
+// theme -> It defines colors , font-size , media-queries , etc.
+// extend ->  If you’d like to preserve the default values for a theme option but also add new values, add your extensions under the extend key in the theme section of your configuration file.
+// plugins -> allows to register plugins with tailwind that can be used to generate extra utils , components , etc
+
+// 3. Why do we have .postcssrc file?
+// PostCSS is a Node. js tool that transforms your styles using JavaScript plugins
